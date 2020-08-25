33f9c4eb90b3ae7cba188e2595d07a747f3d6a30
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2020-08-25 09:10:02 +0500

    Configured redux and mirage

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,9 +1,7 @@
-export interface Diary {
+export interface User {
   id?: string;
-  title: string;
-  type: "private" | "public";
-  createdAt?: string;
-  updatedAt?: string;
-  userId?: string;
-  entryIds: string[] | null;
+  username: string;
+  email: string;
+  password?: string;
+  diaryIds: string[] | null;
 }
